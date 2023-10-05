4cbc1d7b5e2d77afc61109ae44f05beee18fa7ff
Songling Lu <38961149+SonglingLu@users.noreply.github.com>
2023-10-05 15:45:44 -0700

    Add level selection and restart function

---
+++ Assets/Scripts/UI Control/LevelSelectionControl.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelSelection : MonoBehaviour
+{    
+    public int level;
+
+    public void SelectLevel() {
+        SceneManager.LoadScene("Level " + level.ToString());
+    }
+}
---
+++ Assets/Scripts/UI Control/MainMenuRender.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenuRender : MonoBehaviour
+{
+    public GameObject mainMenu;
+    public GameObject levelSelection;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        bool showMainMenu = GlobalVariables.getShowMainMenu();
+        mainMenu.SetActive(showMainMenu);
+        levelSelection.SetActive(!showMainMenu);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
