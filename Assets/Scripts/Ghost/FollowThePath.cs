3d110af31862682bb7e3e167895e6a2e25c8fd4f
Songling Lu <38961149+SonglingLu@users.noreply.github.com>
2023-10-05 23:15:16 -0700

    Merge Ghost Movement

---
+++ Assets/Scripts/Ghost/FollowThePath.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+
+public class FollowThePath : MonoBehaviour
+{
+    [SerializeField] private Transform[] waypoints;
+    [SerializeField] private float moveSpeed = 2f;
+    private int waypointIndex = 0;
+
+    private void Start()
+    {
+        if (waypoints.Length > 0)
+        {
+            transform.position = waypoints[waypointIndex].position;
+        }
+    }
+
+    private void Update()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        if (waypointIndex < waypoints.Length)
+        {
+            float step = moveSpeed * Time.deltaTime;
+            transform.position = Vector2.MoveTowards(transform.position, waypoints[waypointIndex].position, step);
+
+            if (Vector2.Distance(transform.position, waypoints[waypointIndex].position) < 0.01f)
+            {
+                waypointIndex++;
+                if (waypointIndex >= waypoints.Length)
+                {
+                    waypointIndex = 0; // Loop back to the first waypoint
+                }
+            }
+        }
+    }
+    
+}
