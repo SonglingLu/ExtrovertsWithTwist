8056ffe0fc5b75dc97e730775cae1e1465c1fc42
Songling Lu <38961149+SonglingLu@users.noreply.github.com>
2023-10-05 21:49:44 -0700

    Merge Draw Function

--- Assets/Scripts/Drawing/DrawPanelControl.cs
+++ Assets/Scripts/Drawing/DrawPanelControl.cs
@@ -2,20 +2,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour
+public class DrawPanelControl : MonoBehaviour
 {
     public GameObject drawingPanel;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
     public void drawToolsOnClick()
     {
         drawingPanel.SetActive(!drawingPanel.activeInHierarchy);
