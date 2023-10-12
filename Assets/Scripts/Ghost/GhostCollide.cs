92ed582181f02119fad636763c51c626d8ae1445
Hyun <hyunkyo@usc.edu>
2023-10-11 18:56:37 -0700

    ghost sight and chase

--- Assets/Scripts/Ghost/GhostCollide.cs
+++ Assets/Scripts/Ghost/GhostCollide.cs
@@ -6,10 +6,13 @@ public class GhostCollide : MonoBehaviour
 {
     public GameObject finishScreen;
     public GameObject loseText;
+    private GhostChase ghostChase;
+    private Rigidbody2D rb;
 
     // Start is called before the first frame update
     void Start()
     {
+        rb = GetComponent<Rigidbody2D>();
         
     }
 
@@ -27,6 +30,11 @@ public class GhostCollide : MonoBehaviour
             Debug.Log("you lose");
             finishScreen.SetActive(true);
             loseText.SetActive(true);
+
+            if (rb != null)
+            {
+                rb.velocity = Vector2.zero;
+            }
         }
     }
 }
