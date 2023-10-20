b298ee51c9d50e123f426c27751919c7dca147b6
YifeiUSC2023 <yli49470@usc.edu>
2023-10-19 20:21:05 -0700

    Scene SetDistrction

--- Assets/Scripts/Distraction/DistractionController.cs
+++ Assets/Scripts/Distraction/DistractionController.cs
@@ -5,19 +5,21 @@ using UnityEngine;
 public class DistractionController : MonoBehaviour
 {
     public GameObject squarePrefab;
-    public int maxSquares = 3;
+    public int maxSquaresExist = 1;
     private int squareCount = 0;
     private int clickTimes = 0;
+    private int maxClickTimes = 3;
 
     public float squareLifetime = 5.0f; // 正方形的生命周期（秒）
 
     void Update()
     {
-        if (clickTimes<maxSquares && squareCount < maxSquares && Input.GetMouseButtonDown(0))
+        if (clickTimes< maxClickTimes && squareCount < maxSquaresExist && Input.GetMouseButtonDown(0))
         {
             Vector3 spawnPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
             spawnPosition.z = 0;
             GameObject newSquare = Instantiate(squarePrefab, spawnPosition, Quaternion.identity);
+            FindAnyObjectByType<ghost2>().objectToThrow = newSquare;
             StartCoroutine(DestroySquareAfterTime(newSquare));
             squareCount++;
             clickTimes++;
